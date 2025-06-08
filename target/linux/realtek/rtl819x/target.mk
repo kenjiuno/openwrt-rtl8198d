9b4c6181b2f85f178d4a8950aa2cd7291fa4c4f4
kenjiuno <h8mastre@gmail.com>
2025-06-08 15:53:45 +0000

    WIP
    Signed-off-by: kenjiuno <h8mastre@gmail.com>

--- target/linux/realtek/rtl819x/target.mk
+++ target/linux/realtek/rtl819x/target.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-only
 ARCH:=mips
-SUBTARGET:=rtl819x
-CPU_TYPE:=4kec
+SUBTARGET:=rtl8198d
+CPU_TYPE:=mips32r2
 BOARD:=realtek
-BOARDNAME:=Realtek MIPS RTL819X
+BOARDNAME:=Realtek MIPS RTL8198D
 
 define Target/Description
-	Build firmware images for Realtek RTL819x based boards.
+	Build firmware images for Realtek RTL8198D based boards.
 endef
