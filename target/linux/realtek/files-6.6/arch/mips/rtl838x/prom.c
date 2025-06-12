75dbdf62c9846dab4f828bbd9f7797de344ce9de
kenjiuno <h8mastre@gmail.com>
2025-06-12 22:02:02 +0000

    WIP
    Signed-off-by: kenjiuno <h8mastre@gmail.com>

--- target/linux/realtek/files-6.6/arch/mips/rtl838x/prom.c
+++ target/linux/realtek/files-6.6/arch/mips/rtl838x/prom.c
@@ -135,6 +135,40 @@ void __init prom_init(void)
 {
 	uint32_t model;
 
+	{
+		uint32_t val;
+
+		pr_inf("Tests for RTL8198D\n");
+
+		#define BSP_GIMR            0xB8003000
+		val = readl(BSP_GIMR);
+		pr_inf("BSP_GIMR %08X\n", (unsigned)val);
+		
+		#define BSP_GISR            0xB8003004
+		val = readl(BSP_GISR);
+		pr_inf("BSP_GISR %08X\n", (unsigned)val);
+
+		#define BSP_IRR0            0xB8003008
+		val = readl(BSP_IRR0);
+		pr_inf("BSP_IRR0 %08X\n", (unsigned)val);
+
+		#define BSP_IRR1            0xB800300C
+		val = readl(BSP_IRR1);
+		pr_inf("BSP_IRR1 %08X\n", (unsigned)val);
+
+		#define BSP_IRR2            0xB8003010
+		val = readl(BSP_IRR2);
+		pr_inf("BSP_IRR2 %08X\n", (unsigned)val);
+
+		#define BSP_IRR3            0xB8003014
+		val = readl(BSP_IRR3);
+		pr_inf("BSP_IRR3 %08X\n", (unsigned)val);
+
+		#define BSP_TC_BASE         0xB8003100
+		val = readl(BSP_TC_BASE);
+		pr_inf("BSP_TC_BASE %08X\n", (unsigned)val);
+	}
+
 	model = sw_r32(RTL838X_MODEL_NAME_INFO);
 	pr_info("RTL838X model is %x\n", model);
 	model = model >> 16 & 0xFFFF;
